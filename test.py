79f237983c9c12f39b6951f998fa20d8c690efc1
Nruta <nruta.choudhari@duke.edu>
2024-09-15 19:40:39 -0400

    initial commit

--- test.py
+++ test.py
@@ -4,34 +4,31 @@ from mylib.make_functions import calculate_mean, calculate_median, calculate_std
 
 # Creating a DataFrame to use for tests
 df = pd.DataFrame({"values": [1, 2, 3, 4, 5]})
+col = "values"
 
 
 def test_calculate_mean():
-    assert calculate_mean(df["values"]) == 3
+    assert calculate_mean(df, col) == 3
 
 
 def test_calculate_median():
-    assert calculate_median(df["values"]) == 3
+    assert calculate_median(df, col) == 3
 
 
 def test_calculate_std_dev():
-    assert calculate_std_dev(df["values"]) == pytest.approx(1.58, rel=1e-2)
+    assert calculate_std_dev(df, col) == pytest.approx(1.58, rel=1e-2)
 
 
 def test_calculate_mean_empty():
     df_empty = pd.DataFrame({"values": []})
-    assert (
-        calculate_mean(df_empty["values"]) is None
-    )  # Assuming you handle empty input with None or some other indicator
+    assert calculate_mean(df_empty, "values") is None
 
 
 def test_calculate_median_single_value():
     df_single = pd.DataFrame({"values": [5]})
-    assert calculate_median(df_single["values"]) == 5
+    assert calculate_median(df_single, "values") == 5
 
 
 def test_calculate_std_dev_single_value():
     df_single = pd.DataFrame({"values": [5]})
-    assert (
-        calculate_std_dev(df_single["values"]) == 0
-    )  # Std deviation of a single value is zero
+    assert calculate_std_dev(df_single, "values") == 0
